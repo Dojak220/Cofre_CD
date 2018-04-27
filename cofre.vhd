7b60f6afe220b83e6311e813872b744b2f35b4ac
Dorgival Rocha <dorgivalfilho10@outlook.com>
2018-04-27 12:36:35 -0300

    Implementando a 'main' do cofre 02

--- cofre.vhd
+++ cofre.vhd
@@ -36,15 +36,82 @@ architecture estrutura of cofre is
 	);
 	end component;
 	
-	signal estado: std_logic_vector(2 downto 0) = "000";
-	signal senha_memoria std_logic_vector(7 downto 0) = "00000000";
-	signal entrada std_logic_vector(7 downto 0);
-	signal resultado_compara std_logic = 0;
-	signal senha_salva std_logic = 0;
-	signal led_bloq, led_abre, led_modo std_logic = 0;
-	signal bloq std_logic = 0;
+	signal estado: std_logic_vector(2 downto 0) = '000';
+	signal estado_aux: std_logic_vector(2 downto 0);
+	signal senha_memoria: std_logic_vector(7 downto 0) = '00000000';
+	signal entrada: std_logic_vector(7 downto 0);
+	signal resultado_compara: std_logic = 0;
+	signal senha_salva: std_logic = 0;
+	signal led_bloq, led_abre, led_modo: std_logic = 0;
+	signal bloq: std_logic = 0;
 	
 	
 	begin
-		process()
+		FSM: process(estado, clk)
+		
+		begin
+			case estado is
+				when 000 =>
+					led_bloq = '0';
+					led_modo = '0';
+					led_abre = '0';
+					
+					if (cs = '0' and clk = '1' and senha_salva = '0') then
+						estado_aux <= 001;
+					if (cs = '1' and clk) = '1' and senha_salva = '1') then
+						estado_aux <=010;
+									
+				when 001 =>
+					senha_memoria <= entrada;
+					senha_salva <= '1';
+					estado_aux <= '000';
+					
+				when 010 =>
+					led_modo = '1';
+					led_abre = '0';
+					led_bloq = '0';
+					
+					if (clk = '1' and cs = '0')
+						estado_aux <= '000';
+					if (clk = '1' and cs = '1')
+						estado_aux <= '011';
+					
+				
+				when 011 =>
+					if (entrada == senha_memoria) then
+						estado_aux <= '100';
+					else then
+						estado_aux <= '101';
+					
+				when 100 =>
+					led_abre = '1'
+					
+					if (clk = '1') then
+						estado_aux <= '010';
+					
+				when 101 =>
+					led_abre = '0';
+					led_bloq = '1';
+					bloq = '1';
+					
+					if (reset = '1') then
+						estado_aux <= '110';
+											
+				when 110 =>
+					led_bloq = '0';
+					bloq = '0';
+					senha_memoria <= '00000000';
+					
+					estado_aux <= '000';
+					
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	
